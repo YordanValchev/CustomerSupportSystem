e7f6b472cc9f9f7bdcee21e2f8a0b6e8576357c9
Yordan Valchev <y.valchev@outlook.com>
2022-11-28 22:28:22 +0200

    Initial settings and migrations

--- CustomerSupportSystem.Infrastructure/Migrations/00000000000000_CreateIdentitySchema.cs
+++ CustomerSupportSystem.Infrastructure/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace CustomerSupportSystem.Data.Migrations
+namespace CustomerSupportSystem.Infrastructure.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
--- CustomerSupportSystem/Program.cs
+++ CustomerSupportSystem/Program.cs
@@ -1,13 +1,16 @@
-using CustomerSupportSystem.Data;
+using CustomerSupportSystem.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString)
+    );
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
